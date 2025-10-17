f609ee4ac923eebb4a46c5d8450b1e6af444ae97
fantasts <fantasts@gmail.com>
2025-10-17 20:35:06 +0800

    Initial Commit

---
+++ 日常安排/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  日常安排
+//
+//  Created by Haifeng on 2025/10/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
